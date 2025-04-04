6f926f7c51753907154c70ad6bcb335813f8829f
Simon <51213509+smn-cds@users.noreply.github.com>
2025-04-04 07:44:07 +0200

    Fix invalid i18n config passed to Astro (#247)

---
+++ .changeset/tiny-nights-show.md
@@ -0,0 +1,5 @@
+---
+"lightnet": patch
+---
+
+Fix invalid i18n config passed to Astro.
--- packages/lightnet/src/astro-integration/integration.ts
+++ packages/lightnet/src/astro-integration/integration.ts
@@ -69,7 +69,7 @@ export function lightnet(lightnetConfig: LightnetConfig): AstroIntegration {
             locales: resolveLocales(config),
             routing: {
               redirectToDefaultLocale: false,
-              prefixDefaultLocale: false,
+              prefixDefaultLocale: true,
               fallbackType: "rewrite",
             },
           },
