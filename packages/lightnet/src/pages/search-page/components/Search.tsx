3e73a03b98fee6d940c1586d07a57d4bc0832296
Simon <lgtnt@protonmail.com>
2025-06-26 15:45:06 +0200

    use fixed row heights

--- packages/lightnet/src/pages/search-page/components/Search.tsx
+++ packages/lightnet/src/pages/search-page/components/Search.tsx
@@ -1,20 +1,9 @@
 import { useWindowVirtualizer } from "@tanstack/react-virtual"
-import { useEffect, useLayoutEffect, useRef } from "react"
+import { useEffect, useLayoutEffect, useRef, useState } from "react"
 
-import Icon from "../../../components/Icon"
-import { detailsPagePath } from "../../../utils/paths"
 import { useSearch } from "../hooks/use-search"
 import type { TranslationKey, Translations } from "../utils/search-translations"
-
-type MediaType = {
-  name: string
-  icon: string
-}
-
-type TranslatedLanguage = {
-  name: string
-  direction: "rtl" | "ltr"
-}
+import ResultRow, { type MediaType, type TranslatedLanguage } from "./ResultRow"
 
 interface Props {
   currentLocale: string | undefined
@@ -36,16 +25,34 @@ export default function ResultList({
   mediaTypes,
 }: Props) {
   const listRef = useRef<HTMLDivElement | null>(null)
+  const [rowHeight, setRowHeight] = useState(208)
 
   const { results, isLoading } = useSearch()
   const virtualizer = useWindowVirtualizer({
     count: results.length,
-    estimateSize: () => 250,
+    estimateSize: () => rowHeight,
     getItemKey: (index) => results[index].id,
     overscan: 2,
     gap: 1,
     scrollMargin: listRef.current?.offsetTop ?? 0,
   })
+
+  useEffect(() => {
+    const updateRowHeight = () => {
+      // This is the fixed row heights that have a responsive breakpoint
+      // If you update here, also update the initial height
+      const newRowHeight = window.matchMedia("(min-width: 640px)").matches
+        ? 250
+        : 208
+      setRowHeight(newRowHeight)
+    }
+    const observer = new ResizeObserver(() => updateRowHeight())
+    observer.observe(document.body)
+    updateRowHeight()
+    return () => {
+      observer.disconnect()
+    }
+  }, [])
 
   // Astro's ClientRouter is writing a incorrect scroll position to
   // history.state.scrollY. We have seen this when having the search results
@@ -108,75 +115,16 @@ export default function ResultList({
                     virtualRow.start - virtualizer.options.scrollMargin
                   }px)`,
                 }}
-                lang={item.language}
               >
-                <a
-                  href={detailsPagePath(currentLocale, {
-                    id: item.id,
-                  })}
-                  ref={virtualizer.measureElement}
-                  data-index={virtualRow.index}
-                  className="group flex overflow-hidden py-6 transition-colors ease-in-out md:rounded-sm md:py-10 md:hover:bg-gray-100"
-                >
-                  <div className="flex h-36 w-36 shrink-0 flex-col items-start justify-center">
-                    <img
-                      className="max-h-36 w-auto max-w-36 rounded-sm object-contain shadow-md"
-                      src={item.image.src}
-                      width={item.image.width}
-                      height={item.image.height}
-                      alt=""
-                      decoding="async"
-                      loading="eager"
-                    />
-                  </div>
-
-                  <div className="ms-5 flex grow flex-col justify-center text-xs sm:ms-8">
-                    <h2 className="mb-1 line-clamp-3 text-sm font-bold text-gray-700 md:mb-3 md:text-base">
-                      <Icon
-                        className={`${mediaTypes[item.type].icon} me-2 align-bottom text-2xl text-gray-700`}
-                        ariaLabel={mediaTypes[item.type].name}
-                      />
-                      <span>{item.title}</span>
-                    </h2>
-                    <div className="mb-3 flex flex-col flex-wrap items-start gap-2 md:flex-row md:items-center md:gap-3">
-                      {!!item.authors?.length && (
-                        <p className="mb-1 md:mb-0 md:text-base">
-                          {item.authors.join(", ")}
-                        </p>
-                      )}
-                      {showLanguage && (
-                        <span className="rounded-lg border border-gray-300 px-2 py-1 text-gray-500">
-                          {languages[item.language].name}
-                        </span>
-                      )}
-                      <ul lang={currentLocale} className="flex flex-wrap gap-1">
-                        {item.categories?.map((category) => (
-                          <li
-                            key={category}
-                            className="rounded-lg bg-gray-200 px-2 py-1 text-gray-600"
-                          >
-                            {categories[category]}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-
-                    <div className="hidden sm:block">
-                      <p
-                        className="line-clamp-3 max-w-screen-sm text-xs"
-                        lang={item.language}
-                        dir={languages[item.language].direction}
-                      >
-                        {item.description}
-                      </p>
-                    </div>
-                  </div>
-                  <Icon
-                    className="mdi--chevron-right md:group-hover:text-primary my-auto me-4 ms-2 hidden shrink-0 text-2xl text-gray-300 sm:block"
-                    flipIcon={direction === "rtl"}
-                    ariaLabel=""
-                  />
-                </a>
+                <ResultRow
+                  item={item}
+                  direction={direction}
+                  showLanguage={showLanguage}
+                  currentLocale={currentLocale}
+                  categories={categories}
+                  languages={languages}
+                  mediaTypes={mediaTypes}
+                />
               </li>
             )
           })}
